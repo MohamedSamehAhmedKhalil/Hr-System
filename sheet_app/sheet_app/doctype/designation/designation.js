92a1962375e6a782db09f86ce471de2802718364
mohamed <mohamed.sameh@isoft.ae>
2025-05-13 14:09:27 +0300

    last commit

--- sheet_app/sheet_app/doctype/designation/designation.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, mohamed and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Designation", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- sheet_app/sheet_app/doctype/employee_report/employee_report_list.js
+++
@@ -1,7 +0,0 @@
-frappe.listview_settings['Employee Report'] = {
-    onload: function(listview) {
-        frappe.realtime.on("refresh_employee_report", function() {
-            listview.refresh(); 
-        });
-    }
-};
